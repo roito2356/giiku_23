4d8f094ffbf2f02bfbb1fbb3cb9e7c4297051fc4
MiKami12345 <sakaki299@gmail.com>
2023-10-14 20:17:20 +0900

    test用View関数

--- app.py
+++ app.py
@@ -2,16 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.secret_key = "your_secret_key"  # セッションのセキュリティキー
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///login.db"
-# db = SQLAlchemy(app)
+app.secret_key = "your_secret_key"  # セッションのセキュリティキー
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
 
-# # ユーザーモデルを定義
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     password = db.Column(db.String(120), nullable=False)
+# ユーザーモデルを定義
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Complete(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    evaluation = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 # テスト用View関数
 @app.route("/test")
