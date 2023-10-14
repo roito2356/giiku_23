2586afd73bb53c942f91c9b88963b90469869a24
Ohno <aitiohno@gmail.com>
2023-10-14 19:20:33 +0900

    roito2356_2

--- project/app.py
+++ project/app.py
@@ -13,21 +13,35 @@ app = Flask(__name__)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
 #     password = db.Column(db.String(120), nullable=False)
 
-# topページView関数
-@app.route("/test")
-def test():
+
+# マイページView関数
+@app.route("/mypage")
+def mypage():
     return render_template("users/mypage.html")
 
-# topページView関数
+
+# topページ(教材一覧)View関数
 @app.route("/")
 def top():
-    return render_template("users/login.html")
+    return render_template("top-page.html")
 
 
 # ユーザー登録ページView関数
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("users/register.html")
+
+
+# 403エラーページの追加
+@app.errorhandler(403)
+def error_403(error):
+    return render_template("error_pages/403.html"), 403
+
+
+# 404エラーページの追加
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("error_pages/404.html"), 404
 
 
 if __name__ == "__main__":
