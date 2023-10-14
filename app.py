05c5e8e66f5939b99fce3caac0b639e1245dd9e6
MiKami12345 <sakaki299@gmail.com>
2023-10-14 17:18:33 +0900

    CSS適用

--- app.py
+++ app.py
@@ -2,15 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # セッションのセキュリティキー
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
-db = SQLAlchemy(app)
+# app.secret_key = "your_secret_key"  # セッションのセキュリティキー
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///login.db"
+# db = SQLAlchemy(app)
 
-# ユーザーモデルを定義
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
 
-with app.app_context():
-    db.create_all()
+# # ユーザーモデルを定義
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     password = db.Column(db.String(120), nullable=False)
+
+
+# topページView関数
+@app.route("/")
+def top():
+    return render_template("users/login.html")
+
+
+# ユーザー登録ページView関数
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("users/register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
